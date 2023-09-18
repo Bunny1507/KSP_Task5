627ef6db195d01b664787466bc8c9aaa9a3dbef7
Bunny1507 <91088145+Bunny1507@users.noreply.github.com>
2023-09-18 09:25:24 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,66 @@
+console.log("Hi Welcome");
+//window.alert("Hey!!");
+let a = 5;
+let b = 6;
+console.log(a+b);
+
+console.log("55"+55);
+console.log(55+'55');
+let   c = Number(10);
+console.log(c);
+
+/*STRING METHODS
+String length
+String slice()
+String substring()
+String substr()
+String replace()
+String replaceAll()
+String toUpperCase()
+String toLowerCase()
+String concat()	String trim()*/
+
+let name1 = 'Mani';
+let name2 = 'sriramoju';
+
+//String length
+
+console.log(name1.length);  // 4
+
+//String slice()
+
+console.log(name1.slice(0,3));  //Man
+
+//String substring()
+
+console.log(name1.substring(0,2));
+
+//String replace()
+let text = "       Hello World!        ";
+let result1 = text.replace(/^\s+|\s+$/gm,'');
+
+//String replaceAll()
+
+
+// String toUpperCase()
+console.log(name3.toUpperCase())
+
+
+
+// String toLowerCase()
+console.log(name3.toLowerCase())
+
+
+// String concat()
+console.log(name1.concat(name3));
+
+console.log(name3.replace("hii"));
+
+
+//String trim()
+let name5 = "      kanta";
+let result = name5.trim();
+console.log(result)
+
+
+
